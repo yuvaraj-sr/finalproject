ed90ad50502740d1efa0c30922fae4dfd7048c38
YUVARAJ.S <125637632+yuvaraj-sr@users.noreply.github.com>
2023-11-28 15:21:01 +0530

    Add files via upload

---
+++ ads.py
@@ -0,0 +1,20 @@
+css_string = """
+             <style>
+
+            .sidebar {max-width:100%; float:right}
+            .side-block img {width:100%}
+            .side-block {margin-bottom:15px}
+            .side-block a.button {margin-bottom:30px; background: #006CFF; color: #fff;font-weight: 500;font-size: 16px;line-height: 20px;padding: 15px;display: inline-block;max-width: 300px;border-radius: 5px; text-decoration:none;   }
+            .side-block a.button:hover {background:#000}
+
+            </style> 
+
+            <div class="sidebar">
+
+            <div class="side-block">
+            <a target="_blank" href="https://vit.ac.in" rel="noopener">
+            <img src="https://upload.wikimedia.org/wikipedia/en/thumb/c/c5/Vellore_Institute_of_Technology_seal_2017.svg/1200px-Vellore_Institute_of_Technology_seal_2017.svg.png" style="width: 200px; height: 200px;" >
+            </div>
+           
+
+            """
